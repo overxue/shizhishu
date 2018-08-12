e30ee044d9ca46c600aa8a5201e9255e3fbbe7b4
XueCong <xuecong98@gmail.com>
2018-08-12 23:34:27 +0800

    Create categories

---
+++ database/migrations/2018_08_12_230926_seed_categories_data.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '新鲜蔬菜',
+                'iconfont' => '&#xe60e;',
+            ],
+            [
+                'name'        => '鲜肉蛋禽',
+                'iconfont' => '&#xe656;',
+            ],
+            [
+                'name'        => '蒸鲜荟萃',
+                'iconfont' => '&#xe614;',
+            ],
+            [
+                'name'        => '豆制品',
+                'iconfont' => '&#xe610;',
+            ],
+            [
+                'name'        => '米面杂粮',
+                'iconfont' => '&#xe61b;',
+            ],
+            [
+                'name'        => '粮油调料',
+                'iconfont' => '&#xe60f;',
+            ],
+            [
+                'name'        => '酒水饮料',
+                'iconfont' => '&#xe60b;',
+            ],
+            [
+                'name'        => '水果世界',
+                'iconfont' => '&#xe61d;',
+            ]
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
