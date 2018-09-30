15246e7b619a62d4037405043224cb58cc4c0f7f
XueCong <xuecong98@gmail.com>
2018-09-30 15:14:50 +0800

    Coupon api back created_at

--- app/Transformers/CouponTransformer.php
+++ app/Transformers/CouponTransformer.php
@@ -10,12 +10,13 @@ class CouponTransformer extends TransformerAbstract
     public function transform(Coupon $coupon)
     {
         $used = isset($coupon->pivot->is_used) ? $coupon->pivot->is_used : 0;
+        $satarTime = isset($coupon->pivot->created_at) ? $coupon->pivot->created_at->toDateString() : 0;
         return [
             'id' => $coupon->id,
             'money' => str_replace('.00', '', $coupon->money),
             'min_amount' => str_replace('.00', '', $coupon->min_amount),
             'description' => $coupon->description,
-            'satarTime' => $coupon->not_before->toDateString(),
+            'satarTime' => $satarTime,
             'expirAt' => $coupon->not_after->toDateString(),
             'isUsed' => $used,
         ];
